162004feb6b0450cd2e0e1e08e007d06a949daf6
Konstantin Tarkus <hello@tarkus.me>
2021-04-09 20:16:11 +0300

    Re-export existing types from simple-oauth2

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
 /**
  * @copyright 2021-present Kriasoft (https://git.io/JOevo)
  */
+export type { AccessToken, AuthorizationTokenConfig, ClientCredentials, ClientCredentialTokenConfig, ModuleOptions, PasswordTokenConfig, ResourceOwnerPassword, TokenType, WreckHttpOptions, } from "simple-oauth2";
 export { AuthorizationCode as FacebookAuthorizationCode } from "./facebook";
 export { AuthorizationCode as GitHubAuthorizationCode } from "./github";
--- index.ts
+++ index.ts
@@ -2,5 +2,16 @@
  * @copyright 2021-present Kriasoft (https://git.io/JOevo)
  */
 
+export type {
+  AccessToken,
+  AuthorizationTokenConfig,
+  ClientCredentials,
+  ClientCredentialTokenConfig,
+  ModuleOptions,
+  PasswordTokenConfig,
+  ResourceOwnerPassword,
+  TokenType,
+  WreckHttpOptions,
+} from "simple-oauth2";
 export { AuthorizationCode as FacebookAuthorizationCode } from "./facebook";
 export { AuthorizationCode as GitHubAuthorizationCode } from "./github";
